164915fbf7112c94751352469ea31f182720eaf1
Jack Ireland <jackireland@gmail.com>
2018-08-24 09:59:50 -0400

    better time designation and outputs

--- observer_sees_planets.py
+++ observer_sees_planets.py
@@ -7,6 +7,7 @@
 # 
 #
 import json
+import os
 
 import numpy as np
 
@@ -16,14 +17,18 @@ import astropy.units as u
 
 from sunpy.coordinates import frames
 
+#
+directory = os.path.expanduser('~/hvp/hvorgobjects/output/json')
 
-
-# Time we are interested in
-start_time = Time('2000-05-10 00:00:00')
-end_time = Time('2000-05-11 00:00:00')
+# Days we want to calculate positions for
+initial_time = Time('2000-05-10 00:00:00')
+n_days = 7
 
 # Time step
 dt = 30*u.minute
+
+# duration
+duration = 1 * u.day
 
 # Where are we looking from - the observer
 observer_name = 'earth'
@@ -32,26 +37,44 @@ observer_name = 'earth'
 body_names = ('mercury', 'saturn', 'jupiter', 'venus')
 
 
-#
+# Format the output time as requested.
 def format_time_output(t):
     """
-    Format the time output
+    Format the time output.
     """
     return int(np.rint(t.unix * 1000))
 
 
+def file_name_format(observer_name, body_name, t):
+    """
+
+    observer_name :
+    body_name :
+    t :
+
+    Returns
+    -------
+
+    """
+    return '{:s}_{:s}_{:n}.json'.format(observer_name, body_name, format_time_output(t))
+
+# Go through each of the bodies
 for body_name in body_names:
 
-    for nday in range(0, 1):
-        day_start = start_time + nday*u.day
-        start_time_unix = format_time_output(day_start)
+    # Pick the next start time
+    for n in range(0, n_days):
+        start_time = initial_time + n*u.day
 
+        # Reset the positions directory for the new start time
         positions = dict()
-        positions[body_name] = dict()
-        positions[body_name][observer_name] = dict()
+        positions[observer_name] = dict()
+        positions[observer_name][body_name] = dict()
 
-        t = day_start
-        while t - day_start < 1 * u.day:
+        # Reset the counter to the new start time
+        t = start_time
+
+        # Calculate the positions in the duration
+        while t - start_time < duration:
             # The location of the observer
             observer_location = get_body(observer_name, t)
 
@@ -63,14 +86,17 @@ for body_name in body_names:
 
             # time index is unix time stamp in milliseconds - cast to ints
             t_index = format_time_output(t)
-            positions[body_name][observer_name][t_index] = dict()
+            positions[observer_name][body_name][t_index] = dict()
 
-            # todo  -  Positions should be saved as float
-            positions[body_name][observer_name][t_index]["x"] = position.Tx.value
-            positions[body_name][observer_name][t_index]["y"] = position.Ty.value
+            # store the positions of the
+            positions[observer_name][body_name][t_index]["x"] = position.Tx.value
+            positions[observer_name][body_name][t_index]["y"] = position.Ty.value
 
-            t = t + dt
+            # Move the counter forward
+            t += dt
 
-        f = open('{:s}_{:s}_{:n}.json'.format(observer_name, body_name, start_time_unix), 'w')
+        # Open the JSON file and dump out the positional information
+        file_path = os.path.join(directory, file_name_format(observer_name, body_name, start_time))
+        f = open(file_path, 'w')
         json.dump(positions, f)
         f.close()
